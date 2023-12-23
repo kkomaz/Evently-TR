2ccdf2af56ac7427725b50c2a033dcbfc56b855c
Alexander Lee <leeajl006@gmail.com>
2023-12-22 20:54:46 -0800

    Complete header and mobile nav 46:02

--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+'use client';
+
+import { headerLinks } from '@/constants';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const NavItems = () => {
-  return <div>NavItems</div>;
+  const pathname = usePathname();
+
+  return (
+    <ul className="md:flex-between flex flex-col w-full items-start gap-5 md:flex-row">
+      {headerLinks.map((headerLink) => {
+        const isActive = pathname === headerLink.route;
+
+        return (
+          <li
+            key={headerLink.label}
+            className={`
+            ${isActive && 'text-primary-500'}
+            flex-center p-medium-16 whitespace-nowrap
+            `}
+          >
+            <Link href={headerLink.route}>{headerLink.label}</Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default NavItems;
