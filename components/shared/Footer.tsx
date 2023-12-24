96f4670d6cdb77cebb9e73ef4df9fc35c8f81582
Alexander Lee <leeajl006@gmail.com>
2023-12-23 20:17:11 -0800

    Header

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="border-t">
+      <div className="wrapper flex-between flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={128}
+            height={38}
+          />
+        </Link>
+        <p>2023 Evently. All rights reserved</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
