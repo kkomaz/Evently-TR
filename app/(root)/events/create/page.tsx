fc288ddc785befbb815a68f939a7cdc403c75e53
Alexander Lee <leeajl006@gmail.com>
2023-12-25 22:07:14 -0800

    Categories basics

--- app/(root)/events/create/page.tsx
+++ app/(root)/events/create/page.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import EventForm from '@/components/shared/EventForm';
+import { auth } from '@clerk/nextjs';
 
 const CreateEvent = () => {
+  const { sessionClaims } = auth();
+
+  console.log(sessionClaims);
+
+  const userId = sessionClaims?.userId as string;
+
+  console.log(userId, '::userId');
+
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
@@ -11,7 +20,7 @@ const CreateEvent = () => {
       </section>
 
       <div className="wrapper my-8">
-        <EventForm />
+        <EventForm userId={userId} type="Create" />
       </div>
     </>
   );
